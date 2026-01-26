b27929600c65bc1aded2096cadb70b43c7e8d576
blgfml <bloggertai47592@gmail.com>
2026-01-26 18:16:19 +0800

    Site updated: 2026-01-26 18:16:17

--- js/digimon-data.js
+++ js/digimon-data.js
@@ -2,43 +2,98 @@
 
 window.DigimonData = {
     // --- TIME CONFIGURATION ---
-    requirements: { 
-        PHASE_0: 5,  PHASE_1: 3, PHASE_2: 3, PHASE_3: 3,
+    requirements: {
+        PHASE_0: 5, PHASE_1: 3, PHASE_2: 3, PHASE_3: 3,
         PHASE_4: 3, PHASE_5: 3, PHASE_6: 3, PHASE_7: 3
     },
 
     // --- PET DATA ---
     petLines: [
         {
-            name: "AGU-LINE",
+            name: "Agumon",
             sprites: {
-                PHASE_0: ["https://humulos.com/digimon/images/dot/vbdm/agu.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/agu.gif"],
-                PHASE_1: ["https://humulos.com/digimon/images/dot/vbdm/metalgrey_va.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/metalgrey_va.gif"],
-                PHASE_2: ["https://humulos.com/digimon/images/dot/vbdm/omega.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/omega.gif"],
-                PHASE_3: [
-                    ["https://humulos.com/digimon/images/dot/vbdm/greymon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/greymon.gif"],
-                    ["https://humulos.com/digimon/images/dot/vbdm/tyrannomon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/tyrannomon.gif"]
+                PHASE_0: ["https://humulos.com/digimon/images/dot/vbdm/bota.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/bota.gif"],
+                PHASE_1: ["https://humulos.com/digimon/images/dot/vbdm/koro.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/koro.gif"],
+                PHASE_2: [["https://humulos.com/digimon/images/dot/vbdm/agu.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/agu.gif"],
+                ["https://humulos.com/digimon/images/dot/vbdm/agu_b.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/agu_b.gif"]
                 ],
-                PHASE_4: ["https://humulos.com/digimon/images/dot/vbdm/metalgrey_va.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/metalgrey_va.gif"],
-                PHASE_5: ["https://humulos.com/digimon/images/dot/vbdm/wargreymon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/wargreymon.gif"],
+                PHASE_3: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/grey.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/grey.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/monochro.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/monochro.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/grey_b.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/grey_b.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/darktyrano.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/darktyrano.gif"]
+                ],
+                PHASE_4: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/metalgrey_va.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/metalgrey_va.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/vermilli.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/vermilli.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/skullgrey.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/skullgrey.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/metalgrey_vi.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/metalgrey_vi.gif"]
+                ],
+                PHASE_5: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/wargrey.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/wargrey.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/victorygrey.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/victorygrey.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/blackwargrey.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/blackwargrey.gif"]
+                ],
                 PHASE_6: ["https://humulos.com/digimon/images/dot/vbdm/omega.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/omega.gif"],
-                PHASE_7: ["https://humulos.com/digimon/images/dot/vbdm/omega_as.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/omega_as.gif"]
+                PHASE_7: ["https://humulos.com/digimon/images/dot/vbdm/omega_z.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/omega_z.gif"]
             }
         },
         {
-            name: "GABU-LINE",
+            name: "V-mon",
             sprites: {
-                PHASE_0: ["https://humulos.com/digimon/images/dot/vbdm/gabu.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/gabu.gif"],
-                PHASE_1: ["https://humulos.com/digimon/images/dot/vbdm/punimon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/punimon.gif"],
-                PHASE_2: ["https://humulos.com/digimon/images/dot/vbdm/tsunomon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/tsunomon.gif"],
-                PHASE_3: ["https://humulos.com/digimon/images/dot/vbdm/gabumon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/gabumon.gif"],
-                PHASE_4: ["https://humulos.com/digimon/images/dot/vbdm/garurumon.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/garurumon.gif"],
-                PHASE_5: ["https://humulos.com/digimon/images/dot/vbdm/weregaruru.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/weregaruru.gif"],
-                PHASE_6: [
-                    ["https://humulos.com/digimon/images/dot/vbdm/metalgaruru.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/metalgaruru.gif"],
-                    ["https://humulos.com/digimon/images/dot/vbdm/cresgaruru.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/cresgaruru.gif"]
+                PHASE_0: ["https://humulos.com/digimon/images/dot/vbdm/chico.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/chico.gif"],
+                PHASE_1: ["https://humulos.com/digimon/images/dot/vbdm/chibi.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/chibi.gif"],
+                PHASE_2: ["https://humulos.com/digimon/images/dot/vbdm/v.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/v.gif"],
+                PHASE_3: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/vdra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/vdra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/xv.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/xv.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/fladra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/fladra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/lighdra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/lighdra.gif"]
                 ],
-                PHASE_7: ["https://humulos.com/digimon/images/dot/vbdm/omega_zw.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/omega_zw.gif"]
+                PHASE_4: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/aerovdra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/aerovdra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/paildra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/paildra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/sagittari.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/sagittari.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/dinobee.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/dinobee.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/goldvdra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/goldvdra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/digitama.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/digitama.gif"]
+                ],
+
+                PHASE_5: [
+                    ["https://humulos.com/digimon/images/dot/vbdm/ulforcevdra.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/ulforcevdra.gif"],
+                    ["https://humulos.com/digimon/images/dot/vbdm/magna.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/magna.gif"],
+                ],
+
+                PHASE_6: [["https://humulos.com/digimon/images/dot/vbdm/imperialdra_d.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/imperialdra_d.gif"],
+                ["https://humulos.com/digimon/images/dot/vbdm/imperialdra_f.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/imperialdra_f.gif"]],
+                PHASE_7: ["https://humulos.com/digimon/images/dot/d3c/imperialdra_p.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/imperialdra_p.gif"]
+
+            }
+        },
+        {
+            name: "Guilmon",
+            sprites: {
+                PHASE_0: ["https://humulos.com/digimon/images/dot/vbdm/jyari.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/jyari.gif"],
+                PHASE_1: ["https://humulos.com/digimon/images/dot/vbdm/gigi.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/gigi.gif"],
+                PHASE_2: ["https://humulos.com/digimon/images/dot/vbdm/guil.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/guil.gif"],
+                PHASE_3: ["https://humulos.com/digimon/images/dot/vbdm/frame2/grow.gif", "https://humulos.com/digimon/images/dot/vbdm/grow.gif"],
+                PHASE_4: ["https://humulos.com/digimon/images/dot/vbdm/megalogrow.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/megalogrow.gif"],
+                PHASE_5: ["https://humulos.com/digimon/images/dot/vbdm/duke.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/duke.gif"],
+                PHASE_6: ["https://humulos.com/digimon/images/dot/vbdm/medievalduke.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/medievalduke.gif"],
+                PHASE_7: ["https://humulos.com/digimon/images/dot/vbdm/duke_x.gif", "https://humulos.com/digimon/images/dot/vbdm/frame2/duke_x.gif"]
+            }
+        },
+        {
+            name: "Tailmon",
+            sprites: {
+                PHASE_0: ["https://humulos.com/digimon/images/dot/penc/yukimibota.gif", "https://humulos.com/digimon/images/dot/penc/frame2/yukimibota.gif"],
+                PHASE_1: ["https://humulos.com/digimon/images/dot/d3c/plot.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/plot.gif"],
+                PHASE_2: ["https://humulos.com/digimon/images/dot/d3c/tail.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/tail.gif"],
+                PHASE_3: ["https://humulos.com/digimon/images/dot/d3c/neferti.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/neferti.gif"],
+                PHASE_4: ["https://humulos.com/digimon/images/dot/d3c/silphy.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/silphy.gif"],
+                PHASE_5: ["https://humulos.com/digimon/images/dot/d3c/angewo.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/angewo.gif"],
+                PHASE_6: ["https://humulos.com/digimon/images/dot/d3c/holydra.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/holydra.gif"],
+                PHASE_7: ["https://humulos.com/digimon/images/dot/d3c/angewo.gif", "https://humulos.com/digimon/images/dot/d3c/frame2/angewo.gif"]
             }
         }
     ],
--- js/digimon.js
+++ js/digimon.js
@@ -5,14 +5,14 @@ class DigimonGame {
     // 1. LOAD EXTERNAL DATA
     const data = window.DigimonData;
     if (!data) {
-        console.error("Digimon Data file missing!");
-        return;
+      console.error("Digimon Data file missing!");
+      return;
     }
 
     this.requirements = data.requirements;
     this.petLines = data.petLines;
     this.skins = data.skins;
-    
+
     // --- UPDATED: FORCE UPPERCASE CONVERSION ---
     // This ensures even if you type "Hikari Ver." in the data file, it becomes "HIKARI VER."
     this.skinNames = data.skinNames.map(name => name.toUpperCase());
@@ -20,22 +20,22 @@ class DigimonGame {
     // --- STATE INITIALIZATION ---
     this.currentPetIndex = 0;
     this.images = this.petLines[0].sprites;
-    this.currentPhaseImages = this.images['PHASE_0']; 
+    this.currentPhaseImages = this.images['PHASE_0'];
 
     this.phases = ['PHASE_0', 'PHASE_1', 'PHASE_2', 'PHASE_3', 'PHASE_4', 'PHASE_5', 'PHASE_6', 'PHASE_7'];
     this.currentPhaseIndex = 0;
-    
-    this.gameState = 'GAME'; 
+
+    this.gameState = 'GAME';
     this.isPlaying = false;
     this.totalSeconds = 0;
     this.timeAtLastEvolution = 0;
-    
+
     // Menu Config
     this.menuOptions = ["SKIN", "PET", "CANCEL"];
     this.menuIndex = 0;
     this.skinSelectionIndex = 0;
     this.petSelectionIndex = 0;
-    this.isMenuAnimating = false; 
+    this.isMenuAnimating = false;
 
     // Frame Animation
     this.frameIndex = 0;
@@ -52,7 +52,7 @@ class DigimonGame {
       modal: document.getElementById('evolve-modal'),
       menuOverlay: document.getElementById('menu-overlay')
     };
-    
+
     this.startAnimation();
     this.updateDisplay();
   }
@@ -60,185 +60,185 @@ class DigimonGame {
   // --- ANIMATION ENGINE ---
   startAnimation() {
     if (this.animationInterval) clearInterval(this.animationInterval);
-    this.animationInterval = setInterval(() => { this.toggleFrame(); }, 500); 
+    this.animationInterval = setInterval(() => { this.toggleFrame(); }, 500);
   }
 
   toggleFrame() {
     this.frameIndex = this.frameIndex === 0 ? 1 : 0;
-    
+
     if (this.gameState === 'MENU_PET') {
-        // Preview Phase 6 (Mega)
-        const previewSprites = this.petLines[this.petSelectionIndex].sprites['PHASE_6'];
-        if (Array.isArray(previewSprites[0])) {
-            this.els.img.src = previewSprites[0][this.frameIndex];
-        } else {
-            this.els.img.src = previewSprites[this.frameIndex];
-        }
+      // Preview Phase 6 (Mega)
+      const previewSprites = this.petLines[this.petSelectionIndex].sprites['PHASE_5'];
+      if (Array.isArray(previewSprites[0])) {
+        this.els.img.src = previewSprites[0][this.frameIndex];
+      } else {
+        this.els.img.src = previewSprites[this.frameIndex];
+      }
     } else {
-        this.els.img.src = this.currentPhaseImages[this.frameIndex];
+      this.els.img.src = this.currentPhaseImages[this.frameIndex];
     }
   }
 
   // --- INPUTS ---
   handleRightTopInput() {
     if (this.evolutionPending) { this.triggerEvolution(); return; }
-    if (this.isMenuAnimating) return; 
+    if (this.isMenuAnimating) return;
 
     switch (this.gameState) {
-        case 'GAME': this.enterMenu(); break;
-        case 'MENU_MAIN':
-            if (this.menuIndex === 0) this.enterSkinMenu();
-            else if (this.menuIndex === 1) this.enterPetMenu();
-            else this.exitMenu();
-            break;
-        case 'MENU_SKIN': this.applySkin(this.skinSelectionIndex); this.exitMenu(); break;
-        case 'MENU_PET': this.applyPet(this.petSelectionIndex); this.exitMenu(); break;
+      case 'GAME': this.enterMenu(); break;
+      case 'MENU_MAIN':
+        if (this.menuIndex === 0) this.enterSkinMenu();
+        else if (this.menuIndex === 1) this.enterPetMenu();
+        else this.exitMenu();
+        break;
+      case 'MENU_SKIN': this.applySkin(this.skinSelectionIndex); this.exitMenu(); break;
+      case 'MENU_PET': this.applyPet(this.petSelectionIndex); this.exitMenu(); break;
     }
   }
 
   handleUpInput() {
-    if (this.isMenuAnimating) return; 
+    if (this.isMenuAnimating) return;
     switch (this.gameState) {
-        case 'GAME': if(this.isPlaying) this.flashMessage("STATUS: HAPPY"); break;
-        case 'MENU_MAIN': this.navigateMenu(-1, 'menuIndex', this.menuOptions); break;
-        case 'MENU_SKIN': this.navigateMenu(-1, 'skinSelectionIndex', this.skinNames); break;
-        case 'MENU_PET':
-            this.petSelectionIndex--;
-            if (this.petSelectionIndex < 0) this.petSelectionIndex = this.petLines.length - 1;
-            this.updatePetMenuDisplay();
-            break;
+      case 'GAME': if (this.isPlaying) this.flashMessage("STATUS: HAPPY"); break;
+      case 'MENU_MAIN': this.navigateMenu(-1, 'menuIndex', this.menuOptions); break;
+      case 'MENU_SKIN': this.navigateMenu(-1, 'skinSelectionIndex', this.skinNames); break;
+      case 'MENU_PET':
+        this.petSelectionIndex--;
+        if (this.petSelectionIndex < 0) this.petSelectionIndex = this.petLines.length - 1;
+        this.updatePetMenuDisplay();
+        break;
     }
   }
 
   handleDownInput() {
-    if (this.isMenuAnimating) return; 
+    if (this.isMenuAnimating) return;
     switch (this.gameState) {
-        case 'GAME': if(this.isPlaying) this.flashMessage("STATUS: HUNGRY"); break;
-        case 'MENU_MAIN': this.navigateMenu(1, 'menuIndex', this.menuOptions); break;
-        case 'MENU_SKIN': this.navigateMenu(1, 'skinSelectionIndex', this.skinNames); break;
-        case 'MENU_PET':
-            this.petSelectionIndex++;
-            if (this.petSelectionIndex >= this.petLines.length) this.petSelectionIndex = 0;
-            this.updatePetMenuDisplay();
-            break;
+      case 'GAME': if (this.isPlaying) this.flashMessage("STATUS: HUNGRY"); break;
+      case 'MENU_MAIN': this.navigateMenu(1, 'menuIndex', this.menuOptions); break;
+      case 'MENU_SKIN': this.navigateMenu(1, 'skinSelectionIndex', this.skinNames); break;
+      case 'MENU_PET':
+        this.petSelectionIndex++;
+        if (this.petSelectionIndex >= this.petLines.length) this.petSelectionIndex = 0;
+        this.updatePetMenuDisplay();
+        break;
     }
   }
 
   handleRightBottomInput() {
     if (this.isMenuAnimating) return;
     switch (this.gameState) {
-        case 'GAME': if (this.isPlaying) this.stop(); else this.start(); break;
-        default: this.exitMenu(); break;
+      case 'GAME': if (this.isPlaying) this.stop(); else this.start(); break;
+      default: this.exitMenu(); break;
     }
   }
 
   // --- MENU LOGIC ---
   navigateMenu(dir, propName, dataArray) {
-      let newIndex = this[propName] + dir;
-      if (newIndex >= dataArray.length) newIndex = 0;
-      if (newIndex < 0) newIndex = dataArray.length - 1;
+    let newIndex = this[propName] + dir;
+    if (newIndex >= dataArray.length) newIndex = 0;
+    if (newIndex < 0) newIndex = dataArray.length - 1;
 
-      const currentText = dataArray[this[propName]];
-      const nextText = dataArray[newIndex];
+    const currentText = dataArray[this[propName]];
+    const nextText = dataArray[newIndex];
 
-      this[propName] = newIndex;
-      this.animateBarTransition(currentText, nextText, dir);
-      
-      if(this.gameState === 'MENU_SKIN') {
-           this.els.container.style.backgroundImage = `url('${this.skins[this[propName]]}')`;
-      }
+    this[propName] = newIndex;
+    this.animateBarTransition(currentText, nextText, dir);
+
+    if (this.gameState === 'MENU_SKIN') {
+      this.els.container.style.backgroundImage = `url('${this.skins[this[propName]]}')`;
+    }
   }
 
   animateBarTransition(oldText, newText, dir) {
-      this.isMenuAnimating = true;
-      const barContainer = document.querySelector('.menu-bar');
-      if(!barContainer) return; 
+    this.isMenuAnimating = true;
+    const barContainer = document.querySelector('.menu-bar');
+    if (!barContainer) return;
 
-      const oldEl = document.createElement('div');
-      oldEl.className = 'menu-text';
-      oldEl.innerText = oldText;
-      
-      const newEl = document.createElement('div');
-      newEl.className = 'menu-text';
-      newEl.innerText = newText;
+    const oldEl = document.createElement('div');
+    oldEl.className = 'menu-text';
+    oldEl.innerText = oldText;
 
-      if (dir === 1) {
-          oldEl.classList.add('slide-out-up');
-          newEl.classList.add('slide-in-up');
-      } else {
-          oldEl.classList.add('slide-out-down');
-          newEl.classList.add('slide-in-down');
-      }
+    const newEl = document.createElement('div');
+    newEl.className = 'menu-text';
+    newEl.innerText = newText;
 
-      barContainer.innerHTML = '';
-      barContainer.appendChild(oldEl);
-      barContainer.appendChild(newEl);
+    if (dir === 1) {
+      oldEl.classList.add('slide-out-up');
+      newEl.classList.add('slide-in-up');
+    } else {
+      oldEl.classList.add('slide-out-down');
+      newEl.classList.add('slide-in-down');
+    }
 
-      setTimeout(() => {
-          this.isMenuAnimating = false;
-          barContainer.innerHTML = `<div class="menu-text">${newText}</div>`;
-      }, 200);
+    barContainer.innerHTML = '';
+    barContainer.appendChild(oldEl);
+    barContainer.appendChild(newEl);
+
+    setTimeout(() => {
+      this.isMenuAnimating = false;
+      barContainer.innerHTML = `<div class="menu-text">${newText}</div>`;
+    }, 200);
   }
 
   renderStaticBar(text) {
-      this.els.menuOverlay.style.display = 'flex'; 
-      this.els.menuOverlay.innerHTML = `<div class="menu-bar"><div class="menu-text">${text}</div></div>`;
+    this.els.menuOverlay.style.display = 'flex';
+    this.els.menuOverlay.innerHTML = `<div class="menu-bar"><div class="menu-text">${text}</div></div>`;
   }
 
-  enterMenu() { 
-      this.gameState = 'MENU_MAIN'; 
-      this.menuIndex = 0; 
-      this.renderStaticBar(this.menuOptions[0]);
-      this.els.msg.innerText = ""; this.els.time.innerText = "";
+  enterMenu() {
+    this.gameState = 'MENU_MAIN';
+    this.menuIndex = 0;
+    this.renderStaticBar(this.menuOptions[0]);
+    this.els.msg.innerText = ""; this.els.time.innerText = "";
   }
-  enterSkinMenu() { 
-      this.gameState = 'MENU_SKIN'; 
-      this.skinSelectionIndex = 0; 
-      this.renderStaticBar(this.skinNames[0]);
+  enterSkinMenu() {
+    this.gameState = 'MENU_SKIN';
+    this.skinSelectionIndex = 0;
+    this.renderStaticBar(this.skinNames[0]);
   }
-  enterPetMenu() { 
-      this.gameState = 'MENU_PET'; 
-      this.petSelectionIndex = this.currentPetIndex; 
-      this.updatePetMenuDisplay(); 
+  enterPetMenu() {
+    this.gameState = 'MENU_PET';
+    this.petSelectionIndex = this.currentPetIndex;
+    this.updatePetMenuDisplay();
   }
   exitMenu() {
-      this.gameState = 'GAME';
-      this.els.menuOverlay.style.display = 'none'; 
-      this.updateDisplay(); 
-      this.frameIndex = 0;
-      this.toggleFrame(); 
+    this.gameState = 'GAME';
+    this.els.menuOverlay.style.display = 'none';
+    this.updateDisplay();
+    this.frameIndex = 0;
+    this.toggleFrame();
   }
   updatePetMenuDisplay() {
-      this.els.menuOverlay.style.display = 'none';
-      this.els.time.innerText = "PICK EGG:";
-      this.els.msg.innerText = "> " + this.petLines[this.petSelectionIndex].name;
+    this.els.menuOverlay.style.display = 'none';
+    this.els.time.innerText = "PICK EGG:";
+    this.els.msg.innerText = "> " + this.petLines[this.petSelectionIndex].name;
   }
 
-  applySkin(index) { 
-      this.els.container.style.backgroundImage = `url('${this.skins[index]}')`; 
-      this.flashMessage("SKIN SET!"); 
+  applySkin(index) {
+    this.els.container.style.backgroundImage = `url('${this.skins[index]}')`;
+    this.flashMessage("SKIN SET!");
   }
 
   applyPet(index) {
-      if (index !== this.currentPetIndex) {
-        this.currentPetIndex = index;
-        this.images = this.petLines[index].sprites;
-        this.currentPhaseIndex = 0; 
-        this.currentPhaseImages = this.images['PHASE_0'];
-        this.totalSeconds = 0;
-        this.timeAtLastEvolution = 0;
-        this.flashMessage("NEW EGG!");
-      } else {
-        this.flashMessage("NO CHANGE");
-      }
+    if (index !== this.currentPetIndex) {
+      this.currentPetIndex = index;
+      this.images = this.petLines[index].sprites;
+      this.currentPhaseIndex = 0;
+      this.currentPhaseImages = this.images['PHASE_0'];
+      this.totalSeconds = 0;
+      this.timeAtLastEvolution = 0;
+      this.flashMessage("NEW EGG!");
+    } else {
+      this.flashMessage("NO CHANGE");
+    }
   }
 
   // --- GAME LOGIC ---
-  start() { if(!this.isPlaying) { this.isPlaying = true; this.els.msg.innerText = "ACTIVE"; this.timerInterval = setInterval(() => { this.tick(); }, 1000); } }
+  start() { if (!this.isPlaying) { this.isPlaying = true; this.els.msg.innerText = "ACTIVE"; this.timerInterval = setInterval(() => { this.tick(); }, 1000); } }
   stop() { this.isPlaying = false; this.els.msg.innerText = "PAUSED"; clearInterval(this.timerInterval); this.els.modal.style.display = 'none'; this.evolutionPending = false; }
-  
+
   tick() {
-    if (this.gameState !== 'GAME') return; 
+    if (this.gameState !== 'GAME') return;
     this.totalSeconds++;
     this.updateDisplay();
     this.checkEvolution();
@@ -248,12 +248,12 @@ class DigimonGame {
     if (this.evolutionPending || !this.isPlaying) return;
     const timeInCurrentPhase = this.totalSeconds - this.timeAtLastEvolution;
     const currentPhaseName = this.phases[this.currentPhaseIndex];
-    if (this.currentPhaseIndex >= this.phases.length - 1) return; 
+    if (this.currentPhaseIndex >= this.phases.length - 1) return;
     if (timeInCurrentPhase >= this.requirements[currentPhaseName]) this.showEvolutionPrompt();
   }
 
   showEvolutionPrompt() { this.evolutionPending = true; this.els.modal.style.display = 'flex'; }
-  
+
   triggerEvolution() {
     this.els.modal.style.display = 'none';
     this.evolutionPending = false;
@@ -266,12 +266,12 @@ class DigimonGame {
 
     // Branching Logic
     if (Array.isArray(potentialSprites[0])) {
-        const randomIndex = Math.floor(Math.random() * potentialSprites.length);
-        this.currentPhaseImages = potentialSprites[randomIndex];
+      const randomIndex = Math.floor(Math.random() * potentialSprites.length);
+      this.currentPhaseImages = potentialSprites[randomIndex];
     } else {
-        this.currentPhaseImages = potentialSprites;
+      this.currentPhaseImages = potentialSprites;
     }
-    
+
     this.els.img.src = this.currentPhaseImages[0];
     this.flashMessage("EVOLVED!");
   }
@@ -285,16 +285,16 @@ class DigimonGame {
   }
 
   flashMessage(text) {
-      const originalText = this.els.msg.innerText;
-      this.els.msg.innerText = text;
-      setTimeout(() => {
-          if (this.gameState === 'GAME') {
-             this.els.msg.innerText = (!this.isPlaying) ? originalText : "ACTIVE";
-          }
-      }, 1500);
+    const originalText = this.els.msg.innerText;
+    this.els.msg.innerText = text;
+    setTimeout(() => {
+      if (this.gameState === 'GAME') {
+        this.els.msg.innerText = (!this.isPlaying) ? originalText : "ACTIVE";
+      }
+    }, 1500);
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    if(document.getElementById('digimon-container')) window.game = new DigimonGame();
+  if (document.getElementById('digimon-container')) window.game = new DigimonGame();
 });
